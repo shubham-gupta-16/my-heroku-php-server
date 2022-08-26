bcfd648545942f6deca737b7447ffdeca86652c2
Shubham Gupta <shubham.developer16@gmail.com>
2022-08-26 08:37:22 +0530

    flutter-ecommerce-demo

---
+++ flutter-ecommerce-demo/index.php
@@ -0,0 +1,133 @@
+<?php
+$page1 = [
+    "status" => 200,
+    "message" => "success",
+    "totalRecord" => 12,
+    "totalPage" => 1,
+    "data" => [
+        [
+            "id" => 1,
+            "slug" => "Apple_iPhone_13",
+            "title" => "Apple iPhone 13",
+            "description" => "Apple iPhone 13 (128GB) - Midnight",
+            "price" => 74900,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/1.jpg",
+            "status" => "1",
+            "created_at" => "2020-12-01T06:28:42.000000Z"
+        ],
+        [
+            "id" => 2,
+            "slug" => "Apple_iPhone_12",
+            "title" => "Apple iPhone 12",
+            "description" => "Apple iPhone 12 (128GB) - Black",
+            "price" => 59999,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/2.jpg",
+            "status" => "1",
+            "created_at" => "2020-08-08T06:34:34.000000Z"
+        ],
+        [
+            "id" => 3,
+            "slug" => "Samsung_Galaxy_Z_Flip3_5G",
+            "title" => "Samsung Galaxy Z Flip3 5G",
+            "description" => "Samsung Galaxy Z Flip3 5G (Cream, 8GB RAM, 128GB Storage) with No Cost EMI/Additional Exchange Offers",
+            "price" => 84999,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/3.jpg",
+            "status" => "1",
+            "created_at" => "2021-08-08T06:34:34.000000Z"
+        ],
+        [
+            "id" => 4,
+            "slug" => "iQOO_9_Pro_5G",
+            "title" => "iQOO 9 Pro 5G",
+            "description" => "iQOO 9 Pro 5G (Dark Cruise, 12GB RAM, 256GB Storage) | Snapdragon 8 Gen 1 Mobile Processor | 120W FlashCharge | Extra Rs.4000 Off on Exchange | Upto 12 Months No Cost EMI",
+            "price" => 69990,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/4.jpg",
+            "status" => "1",
+            "created_at" => "2021-12-24T06:34:34.000000Z"
+        ],
+        [
+            "id" => 5,
+            "slug" => "Armor_11T_5G",
+            "title" => "Armor 11T 5G",
+            "description" => "India Gadgets - Armor 11T 5G Rugged Android 11 Mobile Phone: 8Gb + 256Gb: 48MP Quad Camera with Thermal Imaging Camera: 6.1 HD+ Display: 5200mAh Battery: Waterproof IP68",
+            "price" => 57000,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/5.jpg",
+            "status" => "1",
+            "created_at" => "2020-12-21T06:34:34.000000Z"
+        ],
+        [
+            "id" => 6,
+            "slug" => "V20_5G_Rugged_Android_11",
+            "title" => "V20 5G Rugged Android 11",
+            "description" => "India Gadgets - V20 5G Rugged Android 11 Mobile Phone: 8Gb + 256Gb: 64MP + 20MP Night Vison Camera: 6.43 AMOLED FHD+ Display: 6000mAh Battery 33W + 15W Fast Wireless",
+            "price" => 48500,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/6.jpg",
+            "status" => "1",
+            "created_at" => "2020-03-09T06:34:34.000000Z"
+        ],
+        [
+            "id" => 7,
+            "slug" => "Power_Armor_13_Rugged_Android_11",
+            "title" => "Power Armor 13 Rugged Android 11",
+            "description" => "India Gadgets - Power Armor 13 Rugged Android 11 Mobile Phone: 8Gb + 256Gb: 48MP Quad Camera: 6.81 FHD+ Display: Massive 13200mAh Battery: Waterproof IP68 Smartphone",
+            "price" => 44500,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/7.jpg",
+            "status" => "1",
+            "created_at" => "2021-07-07T06:34:34.000000Z"
+        ],
+        [
+            "id" => 8,
+            "slug" => "V10_5G_Rugged_Android_11",
+            "title" => "V10 5G Rugged Android 11",
+            "description" => "India Gadgets - V10 5G Rugged Android 11 Mobile Phone: 8Gb + 128Gb: 48MP Triple Camera: 6.39 HD+ Display: 8500mAh Battery with OTG Reverse Charging: Waterproof Smartphone",
+            "price" => 38500,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/8.jpg",
+            "status" => "1",
+            "created_at" => "2020-04-04T06:34:34.000000Z"
+        ],
+        [
+            "id" => 9,
+            "slug" => "Google_5A_5G_Mobile Black",
+            "title" => "Google 5A 5G - Mobile Black",
+            "description" => "Released 2021, August 26, 183g, 7.6mm thickness, Android 11, up to Android 12, 128GB storage, no card slot",
+            "price" => 37999,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/9.jpg",
+            "status" => "1",
+            "created_at" => "2021-06-08T06:34:34.000000Z"
+        ],
+        [
+            "id" => 10,
+            "slug" => "Mi_11X_Pro_5G",
+            "title" => "Mi 11X Pro 5G",
+            "description" => "Mi 11X Pro 5G (Lunar White, 8GB RAM, 128GB Storage) | Snapdragon 888 | 108MP Camera | 6 Month Free Screen Replacement for Prime | Extra Offers on Exchange",
+            "price" => 36999,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/10.jpg",
+            "status" => "1",
+            "created_at" => "2021-05-01T06:34:34.000000Z"
+        ],
+        [
+            "id" => 11,
+            "slug" => "S97_Pro_Rugged_Android_11",
+            "title" => "S97 Pro Rugged Android 11",
+            "description" => "India Gadgets - S97 Pro Rugged Android 11 Mobile Phone: 8Gb + 128Gb: 48MP Quad Camera: 6.39 HD+ Display: Built-in Laser Rangefinder: 8500mAh Battery: Waterproof Smartphone (Black)",
+            "price" => 36500,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/11.jpg",
+            "status" => "1",
+            "created_at" => "2022-01-15T06:34:34.000000Z"
+        ],
+        [
+            "id" => 12,
+            "slug" => "Samsung_Galaxy_M12",
+            "title" => "Samsung Galaxy M12",
+            "description" => "Samsung Galaxy M12 (Black,4GB RAM, 64GB Storage) 6000 mAh with 8nm Processor | True 48 MP Quad Camera | 90Hz Refresh Rate",
+            "price" => 11500,
+            "featured_image" => "http://205.134.254.135/~mobile/MtProject/uploads/product_image/12.jpg",
+            "status" => "1",
+            "created_at" => "2021-11-26T06:34:34.000000Z"
+        ]
+    ]
+];
+
+header('Content-Type: application/json; charset=utf-8');
+
+echo json_encode($page1);
