1540756e02d906708eeea54bd921e783bc4557f1
Shubham Gupta <shubham.developer16@gmail.com>
2022-09-01 17:47:03 +0530

    View css added

--- routes.php
+++ routes.php
@@ -3,5 +3,5 @@
 $ROUTES = [
     Router::GET('/flutter-ecommerce-demo/products', 'flutter-ecommerce-demo-products.php', 'index'),
     Router::GET('/test/{log}/{data}', 'flutter-ecommerce-demo-products.php', 'test'),
-    Router::GET('/view-example', 'flutter-ecommerce-demo-products.php', 'view'),
+    Router::GET('/view/example', 'flutter-ecommerce-demo-products.php', 'view'),
 ];
--- views/view-example.php
+++ views/view-example.php
@@ -1,1 +1,16 @@
-This is a example view with data: <?= $DATA['wow'] ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/public/css/main.css" rel="stylesheet">
+    <title>Example</title>
+</head>
+
+<body>
+    This is a example view with data: <?= $DATA['wow'] ?>
+</body>
+
+</html>
