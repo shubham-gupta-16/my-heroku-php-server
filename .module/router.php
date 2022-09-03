80f9b50de99146e4a7aa588737954722ef966fda
Shubham Gupta <shubham.developer16@gmail.com>
2022-09-03 10:37:42 +0530

    Router function important

--- .module/router.php
+++ .module/router.php
@@ -2,40 +2,36 @@
 
 class Router
 {
-
     public $uri;
     public $method;
     public $file;
     public $func;
 
-
-    private function __construct(string $method, string $uri, string $file, ?string $func = null)
+    private function __construct(string $method, string $uri, string $file, string $func)
     {
         $this->method = $method;
         $this->uri = $uri;
         $this->file = $file;
-        if ($func != null) {
-            $this->func = $func;
-        }
+        $this->func = $func;
     }
 
-    static public function GET(string $uri, string $file, ?string $func = null): Router
+    static public function GET(string $uri, string $file, string $func): Router
     {
         return new Router('GET', $uri, $file, $func);
     }
-    static public function POST(string $uri, string $file, ?string $func = null): Router
+    static public function POST(string $uri, string $file, string $func): Router
     {
         return new Router('POST', $uri, $file, $func);
     }
-    static public function PUT(string $uri, string $file, ?string $func = null): Router
+    static public function PUT(string $uri, string $file, string $func): Router
     {
         return new Router('PUT', $uri, $file, $func);
     }
-    static public function PATCH(string $uri, string $file, ?string $func = null): Router
+    static public function PATCH(string $uri, string $file, string $func): Router
     {
         return new Router('PATCH', $uri, $file, $func);
     }
-    static public function DELETE(string $uri, string $file, ?string $func = null): Router
+    static public function DELETE(string $uri, string $file, string $func): Router
     {
         return new Router('DELETE', $uri, $file, $func);
     }
