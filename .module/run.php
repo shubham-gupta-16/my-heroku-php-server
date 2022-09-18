14db27a288d36251fe623179e41c65464d33482c
Shubham Gupta <shubham.developer16@gmail.com>
2022-09-18 14:00:30 +0530

    Finalize

--- .module/run.php
+++ .module/run.php
@@ -13,19 +13,13 @@ header('Access-Control-Allow-Credentials: true');
 
 $uri = $_SERVER['REQUEST_URI'];
 
-// print_r($_SERVER);
 if (isset($ROUTES[$uri])){
     $route = $ROUTES[$uri];
-    // echo $uri;
-    // echo $route->file;
     runpage($route);
 }
 else if ($r = matchRoute($ROUTES)) {
     runpage($r[0], $r[1]);
-    // echo json_encode($data);
-    //  && is_file('../controllers/' . $ROUTES[$uri])
 } else {
-    // Route Not Found
     err();
 }
 
@@ -65,9 +59,6 @@ function err()
 
 function matchRoute($routes = [], $url = null, $method = 'GET')
 {
-    // I used PATH_INFO instead of REQUEST_URI, because the 
-    // application may not be in the root direcory
-    // and we dont want stuff like ?var=value
     $reqUrl = $url ?? $_SERVER['REDIRECT_URL'];
     $reqMet = $method ?? $_SERVER['REQUEST_METHOD'];
 
@@ -76,19 +67,11 @@ function matchRoute($routes = [], $url = null, $method = 'GET')
     foreach ($routes as $uri => $route) {
         if(strpos($uri, '{') === false)
             continue;
-        // convert urls like '/users/:uid/posts/:pid' to regular expression
-        // $pattern = "@^" . preg_replace('/\\\:[a-zA-Z0-9\_\-]+/', '([a-zA-Z0-9\-\_]+)', preg_quote($route['url'])) . "$@D";
         $pattern = "@^" . preg_replace('/{[a-zA-Z0-9\_\-.]+}/', '([a-zA-Z0-9\_\-.]+)', $uri) . "$@D";
-        // echo $pattern."\n";
         $params = [];
-        // check if the current request params the expression
         $match = preg_match($pattern, $reqUrl, $params);
         if ($reqMet == $route->method && $match) {
-            // remove the first match
             array_shift($params);
-
-            // call the callback with the matched positions as params
-            // return call_user_func_array($route['callback'], $params);
             return [$route, $params];
         }
     }
