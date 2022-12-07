e7e8cf41ac010d1d58bea277dba49af2571e6b63
Shubham Gupta <shubham.developer16@gmail.com>
2022-12-07 10:06:11 +0530

    validate request methods

--- public/index.php
+++ public/index.php
@@ -34,24 +34,30 @@ function runpage($route, array $data = [])
     // echo get_class($route);
     switch (get_class($route)) {
         case 'Internal\Types\TypeView':
+            _validate_method($route->method, false);
             if (!is_file("..\\views\\" . $route->file . '.php')) return err(500);
             $DATA = (object) $route->data;
             include '..\\views\\' . $route->file . '.php';
             _output($response->status, $response->headers, null);
+            break;
+        case 'Internal\Types\TypeJson':
+            _validate_method($route->method, true);
+            _output($response->status, $response->headers, $response->data);
             break;
         case 'Internal\Types\TypeController':
             if (!is_file("..\\controllers\\" . $route->file . '.php')) return err(500);
             include '..\\controllers\\' . $route->file . '.php';
             $response = call_user_func($route->fun, ...$data);
             if (gettype($response) == 'object' && get_class($response) == 'Internal\Types\TypeView') {
+                _validate_method($route->method, false);
                 $DATA = (object) $response->data;
                 include '..\\views\\' . $response->file . '.php';
                 _output($response->status, $response->headers, null);
             } elseif (gettype($response) == 'object' && get_class($response) == 'Internal\Types\TypeJson') {
+                _validate_method($route->method, true);
                 _output($response->status, $response->headers, $response->data);
-                // header
-                // status
             } else {
+                _validate_method($route->method, true);
                 _output(200, null, $response);
             }
             break;
@@ -67,7 +73,7 @@ function _output(int $status_code, ?array $headers, $data)
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($data);
     }
-    if ($headers != null){
+    if ($headers != null) {
         foreach ($headers as $header) {
             header($header);
         }
@@ -75,20 +81,30 @@ function _output(int $status_code, ?array $headers, $data)
     die;
 }
 
-function err(int $status_code)
+function _validate_method(string $method, bool $isJson){
+    if ($_SERVER['REQUEST_METHOD'] !== $method) {
+        return err(400, $isJson ? 'Bad Request' : null);
+    }
+}
+
+function err(int $status_code, ?string $message = null, ?array $data = null)
 {
     http_response_code($status_code);
-    // header('Content-Type: application/json; charset=utf-8');
-    // echo json_encode([
-    //     'error' => 'File not found'
-    // ]);
+    if ($message != null){
+        header('Content-Type: application/json; charset=utf-8');
+        $_errorJson = [
+            'error' => $message
+        ];
+        if ($data != null)
+        $_errorJson['data'] = $data;
+        echo json_encode($_errorJson);
+    }
     die;
 }
 
-function matchRoute($routes = [], $url = null, $method = 'GET')
+function matchRoute($routes = [], $url = null)
 {
     $reqUrl = $url ?? $_SERVER['REDIRECT_URL'];
-    $reqMet = $method ?? $_SERVER['REQUEST_METHOD'];
 
     $reqUrl = rtrim($reqUrl, "/");
 
@@ -97,8 +113,7 @@ function matchRoute($routes = [], $url = null, $method = 'GET')
             continue;
         $pattern = "@^" . preg_replace('/{[a-zA-Z0-9\_\-.]+}/', '([a-zA-Z0-9\_\-.]+)', $uri) . "$@D";
         $params = [];
-        $match = preg_match($pattern, $reqUrl, $params);
-        if ($reqMet == $route->method && $match) {
+        if (preg_match($pattern, $reqUrl, $params)) {
             array_shift($params);
             return [$route, $params];
         }
