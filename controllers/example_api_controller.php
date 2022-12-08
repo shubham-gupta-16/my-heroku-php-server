0c0ef2642bb2e2f12f1978e9460096efba5dc798
Shubham Gupta <shubham.developer16@gmail.com>
2022-12-08 18:34:59 +0530

    Project setup done

--- controllers/example_api_controller.php
+++ controllers/example_api_controller.php
@@ -1,11 +1,9 @@
 <?php
 
 use Internal\Response;
-use PHPMailer\PHPMailer\PHPMailer;
 
 function index()
 {
-    $phpmailer = new PHPMailer();
     return Response::json("Hello Laralite API")->status(201)
         ->headers(['Custom-Header: Hello World']);
 }
--- public/index.php
+++ public/index.php
@@ -30,39 +30,44 @@ if (isset($ROUTES[$uri])) {
 
 function runpage($route, array $data = [])
 {
-    // echo "..\\controllers\\" . $route->file . '.php';
-    // echo get_class($route);
-    switch (get_class($route)) {
-        case 'Internal\Types\TypeView':
-            _validate_method($route->method, false);
-            if (!is_file("..\\views\\" . $route->file . '.php')) return err(500);
-            $DATA = (object) $route->data;
-            include '..\\views\\' . $route->file . '.php';
-            _output($response->status, $response->headers, null);
-            break;
-        case 'Internal\Types\TypeJson':
-            _validate_method($route->method, true);
-            _output($response->status, $response->headers, $response->data);
-            break;
-        case 'Internal\Types\TypeController':
-            if (!is_file("..\\controllers\\" . $route->file . '.php')) return err(500);
-            include '..\\controllers\\' . $route->file . '.php';
-            $response = call_user_func($route->fun, ...$data);
-            if (gettype($response) == 'object' && get_class($response) == 'Internal\Types\TypeView') {
+
+    try {
+        // echo "..\\controllers\\" . $route->file . '.php';
+        // echo get_class($route);
+        switch (get_class($route)) {
+            case 'Internal\Types\TypeView':
                 _validate_method($route->method, false);
-                $DATA = (object) $response->data;
-                include '..\\views\\' . $response->file . '.php';
+                if (!is_file("..\\views\\" . $route->file . '.php')) return err(500);
+                $DATA = (object) $route->data;
+                include '..\\views\\' . $route->file . '.php';
                 _output($response->status, $response->headers, null);
-            } elseif (gettype($response) == 'object' && get_class($response) == 'Internal\Types\TypeJson') {
+                break;
+            case 'Internal\Types\TypeJson':
                 _validate_method($route->method, true);
                 _output($response->status, $response->headers, $response->data);
-            } else {
-                _validate_method($route->method, true);
-                _output(200, null, $response);
-            }
-            break;
-        default:
-            return err(500);
+                break;
+            case 'Internal\Types\TypeController':
+                if (!is_file("..\\controllers\\" . $route->file . '.php')) return err(500);
+                include '..\\controllers\\' . $route->file . '.php';
+                $response = call_user_func($route->fun, ...$data);
+                if (gettype($response) == 'object' && get_class($response) == 'Internal\Types\TypeView') {
+                    _validate_method($route->method, false);
+                    $DATA = (object) $response->data;
+                    include '..\\views\\' . $response->file . '.php';
+                    _output($response->status, $response->headers, null);
+                } elseif (gettype($response) == 'object' && get_class($response) == 'Internal\Types\TypeJson') {
+                    _validate_method($route->method, true);
+                    _output($response->status, $response->headers, $response->data);
+                } else {
+                    _validate_method($route->method, true);
+                    _output(200, null, $response);
+                }
+                break;
+            default:
+                return err(500);
+        }
+    } catch (\Throwable $th) {
+        err($th->getCode(), $th->getMessage());
     }
 }
 
@@ -81,7 +86,8 @@ function _output(int $status_code, ?array $headers, $data)
     die;
 }
 
-function _validate_method(string $method, bool $isJson){
+function _validate_method(string $method, bool $isJson)
+{
     if ($_SERVER['REQUEST_METHOD'] !== $method) {
         return err(400, $isJson ? 'Bad Request' : null);
     }
@@ -90,13 +96,13 @@ function _validate_method(string $method, bool $isJson){
 function err(int $status_code, ?string $message = null, ?array $data = null)
 {
     http_response_code($status_code);
-    if ($message != null){
+    if ($message != null) {
         header('Content-Type: application/json; charset=utf-8');
         $_errorJson = [
             'error' => $message
         ];
         if ($data != null)
-        $_errorJson['data'] = $data;
+            $_errorJson['data'] = $data;
         echo json_encode($_errorJson);
     }
     die;
